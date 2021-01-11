c59383e37297563cd36dd0d486fff8dd3fddb3ca
Davin <lindavin@grinnell.edu>
2021-01-10 22:59:50 -0500

    Create react app
    This sets up the react app.

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		<br />
+		<div>
+			This is a GUI wrapper for useful AOU endpoints for the Morningstar 
+			API on the RapidAPI platform. THe link is  
+			<a href="https://rapidapi.com/apidojo/api/morning-star"> here</a>.
+		</div>
+    </div>
+  );
+}
+
+export default App;
