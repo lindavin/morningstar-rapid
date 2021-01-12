82a8258e0300cd4df958421f21da609f20dcf9be
lindavin <lindavin@grinnell.edu>
2021-01-12 12:06:13 -0500

    Display suggestions and add hook to retrieve information on a specific company

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,31 +1,70 @@
 import React from 'react';
 import About from './About';
+import { getSuggestionsByAutocomplete, getCompanyProfile } from '../services/morningstar.service';
 import { useForm } from 'react-hook-form';
 
 import '../styles/main.css';
 
-function Main(){
+function Main() {
 
-	const { register, handleSubmit, errors  } = useForm();
-	const [companyName, setCompanyName] = React.useState("");
+	const { register, handleSubmit, errors } = useForm();
+	const [companyName, setCompanyName] = React.useState('');
+	const [companyProfile, setCompanyProfile] = React.useState();
+	const [suggestions, setSuggestions] = React.useState([]);
+	const [requestError, setRequestError] = React.useState('');
+
 	const onSubmit = (data) => {
 		setCompanyName(data.companyName);
 	};
+
+	React.useEffect(() => {
+		if (companyName) {
+			setRequestError('');
+			getSuggestionsByAutocomplete(companyName)
+				.then(setSuggestions)
+				.catch(error => setRequestError(error.message))
+		}
+	}, [companyName]);
 
 	const blockStyle = {
 		display: 'block',
 		margin: 'auto'
 	}
 
+	const getProfile = (pid) => () => {
+		getCompanyProfile(pid)
+		.then(setCompanyProfile)
+		.catch(error => setRequestError(error.message))
+	}
+
 	return (
 		<div >
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<label style={blockStyle}>Company Name</label>
-				<input name="companyName" ref={register({ required: true  })} style={blockStyle}/>
-				{errors.companyName && <span style={{ }}>This field is required.</span>  }
-				<input type="submit" style={blockStyle}/>
+				<input name="companyName" ref={register({ required: true })} style={blockStyle} />
+				{errors.companyName && <span style={{}}>This field is required.</span>}
+				<input type="submit" style={blockStyle} />
 			</form>
-			{ !errors.companyName && <About name={companyName} />  }
+			{suggestions.length !== 0 && suggestions.map(suggestion => (
+				<div key={suggestion.performanceId} style={blockStyle}>
+					<span>name: {suggestion.name} </span>
+					<button onClick={getProfile}> Get profile </button>
+				</div>
+			)) }
+			{/* { !errors.companyName && <About name={companyName} />  } */}
+			{ requestError && (
+				<div style={{ 
+					backgroundColor: '#ff5050',
+					border: '5px solid red',
+					boxShadow: '0px 0px 5px 5px #ff5050',
+					margin: 'auto',
+					maxWidth: '50%',
+					opacity: '0.8',
+					padding: '5px'
+					}}>
+				{ requestError }
+				</div>
+			) }
 		</div>
 	)
 }
--- client/src/services/morningstar.service.js
+++ client/src/services/morningstar.service.js
@@ -8,6 +8,8 @@ const headers = {
 	"useQueryString": true
 };
 
+// Todo: display description
+// Get description
 async function getSuggestionsByAutocomplete(text){
 	if(!text){
 		throw new Error("User needs to supply text for autocomplete.");
@@ -21,6 +23,29 @@ async function getSuggestionsByAutocomplete(text){
 	return res.data.results;
 }
 
+// performaceId is used by morningstar api
+async function getCompanyProfile(performanceId){
+	if(!performanceId){
+		throw new Error("In order to search we need a valid performanceId.");
+	}
+	const res = await axios.get(`${path}/get-profile`, {
+		headers: headers,
+		params: {
+			"performanceId": performanceId
+		}
+	});
+	if(res.data[performanceId]){
+		const company = res.data[performanceId];
+		return {
+			about: company.companyProfile.value,
+			industry: company.industry.value
+		}
+	} else {
+		throw new Error(`No company corresponding to performanceId ${performanceId} was found.`);
+	}
+}
+
 export {
-	getSuggestionsByAutocomplete
+	getSuggestionsByAutocomplete,
+	getCompanyProfile
 }
